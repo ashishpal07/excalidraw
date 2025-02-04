d262ee18baf94f92a3bf4c157062c0689eaf9b83
ashishpal07 <ashishpal7861@gmail.com>
2025-02-04 09:03:47 +0530

    feat: add JWT authentication to WebSocket connections and update dependencies

--- apps/websocket-backend/src/index.ts
+++ apps/websocket-backend/src/index.ts
@@ -1,13 +1,32 @@
 import { WebSocketServer } from "ws";
+import jwt from "jsonwebtoken";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws, request) => {
   ws.on('error', console.error);
 
   ws.on('message', (data) => {
     console.log("recievec: %s", data);
   });
+  const url = request.url;
+  const queryParams = new URLSearchParams(url?.split('?')[1]);
+  const token = queryParams.get('token') || "";
+  const decode = jwt.verify(token, process.env.JWT_SECRET || 'secretkey');
 
-  ws.send('something');
+  if (typeof decode === 'string') {
+    ws.close();
+    return;
+  }
+
+  if (!decode || decode.user.user) {
+    ws.close();
+    return;
+  }
+
+  ws.on('message', (data) => {
+    ws.send('pong');
+  })
+
+  // ws.send('something');
 });
